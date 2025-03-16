f01b0b04eb475922b77ba3f72b1122189dc2457b
Ted Lee <dirty4538@gmail.com>
2025-03-16 22:48:50 +0800

    modify web element paser

--- utils.py
+++ utils.py
@@ -43,6 +43,23 @@ def get_web_element_rect(browser, fix_color=True):
     else:
         selected_function = "getRandomColor"
 
+
+    remove_SoM_js = """
+        function removeMarks() {
+            // 查找所有可能的標記元素
+            const markedElements = document.querySelectorAll("div[style*='z-index: 2147483647']");
+            
+            markedElements.forEach(element => {
+                if (element.style.position === "fixed" && element.style.pointerEvents === "none") {
+                    element.remove();
+                }
+            });
+        }
+
+        return removeMarks();
+    """
+    browser.execute_script(remove_SoM_js)
+
     js_script = """
         let labels = [];
 
@@ -52,43 +69,51 @@ def get_web_element_rect(browser, fix_color=True):
             var items = Array.prototype.slice.call(
                 document.querySelectorAll('*')
             ).map(function(element) {
-                var rects = [...element.getClientRects()].map(bb => {
-                    return {
-                        left: bb.left,
-                        top: bb.top,
-                        right: bb.right,
-                        bottom: bb.bottom,
-                        width: bb.width,
-                        height: bb.height
-                    }
+                var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+                var vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+                
+                var rects = [...element.getClientRects()].filter(bb => {
+                var center_x = bb.left + bb.width / 2;
+                var center_y = bb.top + bb.height / 2;
+                var elAtCenter = document.elementFromPoint(center_x, center_y);
+
+                return elAtCenter === element || element.contains(elAtCenter) 
+                }).map(bb => {
+                const rect = {
+                    left: Math.max(0, bb.left),
+                    top: Math.max(0, bb.top),
+                    right: Math.min(vw, bb.right),
+                    bottom: Math.min(vh, bb.bottom)
+                };
+                return {
+                    ...rect,
+                    width: rect.right - rect.left,
+                    height: rect.bottom - rect.top
+                }
                 });
 
                 var area = rects.reduce((acc, rect) => acc + rect.width * rect.height, 0);
 
                 return {
-                    element: element,
-                    include: 
-                        (element.tagName === "INPUT" || element.tagName === "TEXTAREA" || element.tagName === "SELECT") ||
-                        (element.tagName === "BUTTON" || element.tagName === "A" || (element.onclick != null) || window.getComputedStyle(element).cursor == "pointer") ||
-                        (element.tagName === "IFRAME" || element.tagName === "VIDEO" || element.tagName === "LI" || element.tagName === "TD" || element.tagName === "OPTION")
-                    ,
-                    area,
-                    rects,
-                    text: element.textContent.trim().replace(/\s{2,}/g, ' ')
+                element: element,
+                include: 
+                    (element.tagName === "INPUT" || element.tagName === "TEXTAREA" || element.tagName === "SELECT") ||
+                    (element.tagName === "BUTTON" || element.tagName === "A" || (element.onclick != null) || window.getComputedStyle(element).cursor == "pointer") ||
+                    (element.tagName === "IFRAME" || element.tagName === "VIDEO" || element.tagName === "LI" || element.tagName === "TD" || element.tagName === "OPTION")
                 ,
-                isVisible:
-                    !(rects.length === 0) && rects.some(rect => {
-                        return rect.top <= window.innerHeight && 
-                               rect.bottom >= 0 && 
-                               rect.left <= window.innerWidth && 
-                               rect.right >= 0
-                    })
+                area,
+                rects,
+                text: element.textContent.trim().replace(/\s{2,}/g, ' ')
                 };
             }).filter(item =>
                 item.include && (item.area >= 20)
             );
 
+            // Only keep inner clickable items
+            // first delete button inner clickable items
             const buttons = Array.from(document.querySelectorAll('button, a, input[type="button"], div[role="button"]'));
+
+            //items = items.filter(x => !buttons.some(y => y.contains(x.element) && !(x.element === y) ));
             items = items.filter(x => !buttons.some(y => items.some(z => z.element === y) && y.contains(x.element) && !(x.element === y) ));
             items = items.filter(x => 
                 !(x.element.parentNode && 
@@ -99,51 +124,69 @@ def get_web_element_rect(browser, fix_color=True):
 
             items = items.filter(x => !items.some(y => x.element.contains(y.element) && !(x == y)))
 
+            // Function to generate random colors
             function getRandomColor(index) {
                 var letters = '0123456789ABCDEF';
                 var color = '#';
                 for (var i = 0; i < 6; i++) {
-                    color += letters[Math.floor(Math.random() * 16)];
+                color += letters[Math.floor(Math.random() * 16)];
                 }
                 return color;
             }
 
             function getFixedColor(index) {
                 var color = '#000000'
-                return color;
+                return color
             }
+            //function getFixedColor(index){
+            //    var colors = ['#FF0000', '#00FF00', '#0000FF', '#000000']; // Red, Green, Blue, Black
+            //    return colors[index % 4];
+            //}
+            
 
+            // Lets create a floating border on top of these elements that will always be visible
             items.forEach(function(item, index) {
                 item.rects.forEach((bbox) => {
-                    newElement = document.createElement("div");
-                    var borderColor = COLOR_FUNCTION(index);
-                    newElement.style.outline = `2px dashed ${borderColor}`;
-                    newElement.style.position = "absolute";
-                    newElement.style.left = bbox.left + window.scrollX + "px";
-                    newElement.style.top = bbox.top + window.scrollY + "px";
-                    newElement.style.width = bbox.width + "px";
-                    newElement.style.height = bbox.height + "px";
-                    newElement.style.pointerEvents = "none";
-                    newElement.style.boxSizing = "border-box";
-                    newElement.style.zIndex = 2147483647;
-                    
-                    var label = document.createElement("span");
-                    label.textContent = index;
-                    label.style.position = "absolute";
-                    label.style.top = "-19px";
-                    label.style.left = "2px";
-                    label.style.background = borderColor;
-                    label.style.color = "white";
-                    label.style.padding = "2px 4px";
-                    label.style.fontSize = "12px";
-                    label.style.borderRadius = "2px";
-                    newElement.appendChild(label);
-                    
-                    document.body.appendChild(newElement);
-                    labels.push(newElement);
+                newElement = document.createElement("div");
+                var borderColor = COLOR_FUNCTION(index);
+                newElement.style.outline = `2px dashed ${borderColor}`;
+                newElement.style.position = "fixed";
+                newElement.style.left = bbox.left + "px";
+                newElement.style.top = bbox.top + "px";
+                newElement.style.width = bbox.width + "px";
+                newElement.style.height = bbox.height + "px";
+                newElement.style.pointerEvents = "none";
+                newElement.style.boxSizing = "border-box";
+                newElement.style.zIndex = 2147483647;
+                // newElement.style.background = `${borderColor}80`;
+                
+                // Add floating label at the corner
+                var label = document.createElement("span");
+                label.textContent = index;
+                label.style.position = "absolute";
+                //label.style.top = "-19px";
+                label.style.top = Math.max(-19, -bbox.top) + "px";
+                //label.style.left = "0px";
+                label.style.left = Math.min(Math.floor(bbox.width / 5), 2) + "px";
+                label.style.background = borderColor;
+                label.style.color = "white";
+                label.style.padding = "2px 4px";
+                label.style.fontSize = "12px";
+                label.style.borderRadius = "2px";
+                newElement.appendChild(label);
+                
+                document.body.appendChild(newElement);
+                labels.push(newElement);
+                // item.element.setAttribute("-ai-label", label.textContent);
                 });
             })
 
+            // For the first way
+            // return [labels, items.map(item => ({
+            //     rect: item.rects[0] // assuming there's at least one rect
+            // }))];
+
+            // For the second way
             return [labels, items]
         }
         return markPage();""".replace("COLOR_FUNCTION", selected_function)
@@ -156,28 +199,31 @@ def get_web_element_rect(browser, fix_color=True):
         ele_tag_name = items_raw[web_ele_id]['element'].tag_name
         ele_type = items_raw[web_ele_id]['element'].get_attribute("type")
         ele_aria_label = items_raw[web_ele_id]['element'].get_attribute("aria-label")
-        is_visible = "visible" if items_raw[web_ele_id]['isVisible'] == True else "invisible"
-        input_attr_types = ['text', 'search', 'password', 'email', 'tel']
+        ele_name = items_raw[web_ele_id]['element'].get_attribute("name")
+        #is_visible = "visible" if items_raw[web_ele_id]['isVisible'] == True else "invisible"
+        input_attr_types = ['text', 'search', 'password', 'email', 'tel','checkbox','radio']
 
         if not label_text:
             if (ele_tag_name.lower() == 'input' and ele_type in input_attr_types) or ele_tag_name.lower() == 'textarea' or (ele_tag_name.lower() == 'button' and ele_type in ['submit', 'button']):
                 if ele_aria_label:
-                    format_ele_text.append(f"[{web_ele_id}]({is_visible}): <{ele_tag_name}> \"{ele_aria_label}\";")
+                    format_ele_text.append(f"[{web_ele_id}]: <{ele_tag_name}> \"{ele_aria_label}\";")
+                elif ele_name:
+                    format_ele_text.append(f"[{web_ele_id}]: <{ele_tag_name}> \"{ele_name}\";")
                 else:
-                    format_ele_text.append(f"[{web_ele_id}]({is_visible}): <{ele_tag_name}> \"{label_text}\";" )
+                    format_ele_text.append(f"[{web_ele_id}]: <{ele_tag_name}> \"{label_text}\";" )
 
         elif label_text and len(label_text) < 200:
             if not ("<img" in label_text and "src=" in label_text):
                 if ele_tag_name in ["button", "input", "textarea"]:
                     if ele_aria_label and (ele_aria_label != label_text):
-                        format_ele_text.append(f"[{web_ele_id}]({is_visible}): <{ele_tag_name}> \"{label_text}\", \"{ele_aria_label}\";")
+                        format_ele_text.append(f"[{web_ele_id}]: <{ele_tag_name}> \"{label_text}\", \"{ele_aria_label}\";")
                     else:
-                        format_ele_text.append(f"[{web_ele_id}]({is_visible}): <{ele_tag_name}> \"{label_text}\";")
+                        format_ele_text.append(f"[{web_ele_id}]: <{ele_tag_name}> \"{label_text}\";")
                 else:
                     if ele_aria_label and (ele_aria_label != label_text):
-                        format_ele_text.append(f"[{web_ele_id}]({is_visible}): \"{label_text}\", \"{ele_aria_label}\";")
+                        format_ele_text.append(f"[{web_ele_id}]: \"{label_text}\", \"{ele_aria_label}\";")
                     else:
-                        format_ele_text.append(f"[{web_ele_id}]({is_visible}): \"{label_text}\";")
+                        format_ele_text.append(f"[{web_ele_id}]: \"{label_text}\";")
 
 
 
